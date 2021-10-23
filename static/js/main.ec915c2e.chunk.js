11c38c7bd5c20c8afce743927ef8510c4d400a84
RandyMThio40 <37589527+RandyMThio40@users.noreply.github.com>
2021-10-22 22:13:29 -0400

    Updates

---
+++ static/js/main.ec915c2e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-portfolio"]=this["webpackJsonpmy-portfolio"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var l=function(){return Object(s.jsx)("div",{className:"App",children:"hello world!!!"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(l,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.ec915c2e.chunk.js.map
